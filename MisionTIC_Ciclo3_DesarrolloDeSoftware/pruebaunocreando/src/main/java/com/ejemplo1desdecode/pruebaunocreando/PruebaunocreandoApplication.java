0710b4b410ec0aa899626b3c29e01c66f2b9b024
DanilO <djotalorab@gmail.com>
2022-08-16 23:44:00 -0500

    Empezando Ciclo 3

---
+++ MisionTIC_Ciclo3_DesarrolloDeSoftware/pruebaunocreando/src/main/java/com/ejemplo1desdecode/pruebaunocreando/PruebaunocreandoApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo1desdecode.pruebaunocreando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaunocreandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaunocreandoApplication.class, args);
+	}
+
+}
---
+++ MisionTIC_Ciclo3_DesarrolloDeSoftware/pruebaunocreando/src/test/java/com/ejemplo1desdecode/pruebaunocreando/PruebaunocreandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo1desdecode.pruebaunocreando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaunocreandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
